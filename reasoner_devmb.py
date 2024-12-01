4170c99e8b97ac645cb26d9af66551424e0fcac4
Mikel B <mikel.blom@gmail.com>
2024-12-01 15:29:30 +0100

    slight cleanup

--- reasoner_devmb.py
+++ reasoner_devmb.py
@@ -1,6 +1,6 @@
-from py4j.java_gateway import JavaGateway
 from collections import defaultdict
-from itertools import product
+
+from py4j.java_gateway import JavaGateway
 
 # TODO: Need argparser to get the ontology file
 ONTOLOGY_FILE = "pizza.owl"
@@ -37,24 +37,27 @@ allConcepts = ontology.getSubConcepts()
 conceptNames = ontology.getConceptNames()
 # print([formatter.format(x) for x in conceptNames])
 
-# allConcepts should be available as a global i guess? 
+# allConcepts should be available as a global i guess?
 allConcepts = ontology.getSubConcepts()
 
 ## --- // -----------------------------------------------------------------------------
 ## Helper functions for EL Completion Rules
 
+
 def has_top(all_concepts):
     for concept in all_concepts:
         concept_type = concept.getClass().getSimpleName()
         if concept_type == "TopConcept$":
-             return True
-    
+            return True
+
     return False
+
 
 ontology_has_top = has_top(allConcepts)
 
 ## --- // -----------------------------------------------------------------------------
 ## EL Completion Rules
+
 
 def top_rule(individual, interpretation):
     """
@@ -64,16 +67,19 @@ def top_rule(individual, interpretation):
     if ontology_has_top:
         interpretation[individual].add(elFactory.getTop())
         return True
-    
+
     return False
-    
+
+
 def intersect_rule_1(d):
     # Intersect rule 1: If d has C intersection D assigned, assign also C and D to d
     pass
 
+
 def intersect_rule_2(d):
     # Intersect rule 2: If d has C intersection D assigned, assign also C intersect D to d
     pass
+
 
 def exists_rule_1(individual, interpretation, initial_concepts, roles_succesors):
     """
@@ -90,37 +96,33 @@ def exists_rule_1(individual, interpretation, initial_concepts, roles_succesors)
         concept_type = concept.getClass().getSimpleName()
 
         # "If d has Er.C assigned, apply E-rules 1.1 and 1.2"
-        if concept_type == 'ExistentialRoleRestriction':
+        if concept_type == "ExistentialRoleRestriction":
             current_role = concept_type.role()
             current_filler = concept_type.filler()
-            
-            # E-rule 1.1: 
+
+            # E-rule 1.1:
             # If there is an element e with initial concept C assigned, e the r-successor of d.
             if current_filler in initial_concepts:
                 roles_succesors[current_role].add((individual, initial_concepts[current_filler]))
                 changed = True
-                
-            # E-rule 1.2: 
+
+            # E-rule 1.2:
             # Otherwise, add a new r-successor to d, and assign to it as initial concept C.
-            else: 
-                # TODO: resolve this temp fix for indiviual 
+            else:
+                # TODO: resolve this temp fix for indiviual
                 new_individual = "d_1"
                 roles_succesors[current_role].add((individual, new_individual))
                 interpretation[new_individual].add(current_filler)
                 initial_concepts[current_filler] = new_individual
                 changed = True
-                
+
     return changed
 
-
-        
-
-        
-   pass
 
 def exists_rule_2(d):
     # E-rule 2: If d has an r-successor with C assigned, add Er.C to d
     pass
+
 
 def subsumption_rule(d):
     # Subsumption rule: If d has C assigned and C subsumes D, assign D to d
@@ -136,7 +138,7 @@ elFactory = gateway.getELFactory()
 d_0 = "d_0"
 CLASS_NAME = "C"
 
-# Initial concepts: Key = initial concept, Value = individual 
+# Initial concepts: Key = initial concept, Value = individual
 initial_concepts = defaultdict(str)
 
 # interpretation: Key = individual, Value = set of Concepts

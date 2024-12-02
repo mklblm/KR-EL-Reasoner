105663cc9b106658756e5f93ee68290c38878c7c
Mikel B <mikel.blom@gmail.com>
2024-12-02 18:10:36 +0100

    mb dev file: merged class and all rules.

--- reasoner_devmb.py
+++ reasoner_devmb.py
@@ -1,233 +1,231 @@
 from collections import defaultdict
+from itertools import combinations
 
 from py4j.java_gateway import JavaGateway
 
-# TODO: Need argparser to get the ontology file
-ONTOLOGY_FILE = "pizza.owl"
-CLASS_NAME = "C"
 
-## Setup java gateway and load the ontology
-# connect to the java gateway of dl4python
-gateway = JavaGateway()
+class ELReasoner:
+    def __init__(self, ontology_file, class_name) -> None:
+        ## Setup java gateway and load the ontology
+        # connect to the java gateway of dl4python
+        self.gateway = JavaGateway()
 
-# get a parser from OWL files to DL ontologies
-parser = gateway.getOWLParser()
+        # get a parser from OWL files to DL ontologies
+        self.parser = self.gateway.getOWLParser()
 
-# get a formatter to print in nice DL format
-formatter = gateway.getSimpleDLFormatter()
+        # get a formatter to print in nice DL format
+        self.formatter = self.gateway.getSimpleDLFormatter()
 
-## --- // -----------------------------------------------------------------------------
-## Ontology parsing
+        # load an ontology from a file
+        self.ontology = self.parser.parseFile(ontology_file)
 
-# load an ontology from a file
-ontology = parser.parseFile(ONTOLOGY_FILE)
+        # EL algorithm can only handle binary conjunctions
+        self.gateway.convertToBinaryConjunctions(self.ontology)
 
-# EL algorithm can only handle binary conjunctions
-gateway.convertToBinaryConjunctions(ontology)
+        # get the TBox axioms
+        self.tbox = self.ontology.tbox()
+        self.axioms = self.tbox.getAxioms()
 
-# get the TBox axioms
-tbox = ontology.tbox()
-axioms = tbox.getAxioms()
+        # get all concepts occurring in the ontology and print
+        self.all_concepts = self.ontology.getSubConcepts()
 
-# get all concepts occurring in the ontology and print
-allConcepts = ontology.getSubConcepts()
-# print([formatter.format(x) for x in allConcepts])
+        # retrieve concept names and print
+        self.concept_names = self.ontology.getConceptNames()
 
-# retrieve concept names and print
-conceptNames = ontology.getConceptNames()
-# print([formatter.format(x) for x in conceptNames])
+        # to create EL concepts
+        self.el_factory = self.gateway.getELFactory()
 
-# allConcepts should be available as a global i guess?
-allConcepts = ontology.getSubConcepts()
+        ### Attributes to keep track of during runs
+        # store if ontology has top concept
+        self.ontology_contains_top = self.contains_top(self.ontology)
+        self.subsumee = self.elFactory.getConceptName(class_name)
 
-## --- // -----------------------------------------------------------------------------
-## Helper functions for EL Completion Rules
+        # keep track of last individual added (individuals are integers)
+        self.last_individual = 0
+        self.initial_concepts = {}
+        self.blocked_individuals = set()
+        self.interpretation = defaultdict(set)
+        self.role_successors = defaultdict(defaultdict(set))
 
+    def contains_top(self, ontology):
+        all_concepts = ontology.getSubConcepts()
 
-def has_top(all_concepts):
-    for concept in all_concepts:
-        concept_type = concept.getClass().getSimpleName()
-        if concept_type == "TopConcept$":
+        for concept in all_concepts:
+            concept_type = concept.getClass().getSimpleName()
+
+            if concept_type == "TopConcept$":
+                return True
+
+        return False
+
+    # Top Rule: add top to every individual
+    def top_rule(self, individual):
+        """
+        Add top to this individual, only if top occurs in tbox.
+        """
+        if self.ontology_contains_top:
+            self.interpretation[individual].add(self.elFactory.getTop())
             return True
 
-    return False
+        return False
 
+    # Intersect rule 1: If d has C intersection D assigned, assign also C and D to d
+    def intersect_rule_1(self, individual):
+        """
+        Add the conjuncts of all conjunctions assigned to this individual to
+        the individual as well.
+        """
+        changed = False
 
-ontology_has_top = has_top(allConcepts)
+        for concept in self.interpretation[individual]:
+            concept_type = concept.getClass().getSimpleName()
 
-## --- // -----------------------------------------------------------------------------
-## EL Completion Rules
+            # find conjunctions in individual
+            if concept_type == "ConceptConjunction":
+                for conjunct in concept.getConjuncts():
+                    # assign the conjuncts of this conjunction to individual (if not already present)
+                    if conjunct not in self.interpretation[individual]:
+                        self.interpretation[individual].add(conjunct)
+                        changed = True
 
+        return changed
 
-# Top Rule: add top to every individual
-def top_rule(individual, interpretation, ontology_has_top):
-    """
-    Add top to this individual, only if top occurs in tbox.
-    """
-    if ontology_has_top:
-        interpretation[individual].add(elFactory.getTop())
-        return True
+    # Intersect rule 2: If d has C intersection D assigned, assign also C intersect D to d
+    def intersect_rule_2(self, individual):
+        """
+        For all combinations of concepts in individual, also add the conjunction to the
+        individual. Only do this if the conjunction appears in the Tbox.
+        """
+        changed = False
 
-    return False
+        # get all combinations of 2 for the concepts of this individual
+        individual_concepts = list(self.interpretation[individual])
+        all_combinations = combinations(individual_concepts, 2)
 
+        # create a conjunction for all combinations
+        for combination in all_combinations:
+            conjunction = self.elFactory.getConjunction(combination[0], combination[1])
 
-# Intersect rule 1: If d has C intersection D assigned, assign also C and D to d
-def intersect_rule_1(individual, interpretation):
-    """
-    Add the conjuncts of all conjunctions assigned to this individual to
-    the individual as well.
-    """
-    changed = False
+            # assign the conjunction to the individual if it's also in Tbox and not assigned yet
+            if conjunction in self.all_concepts and conjunction not in self.interpretation[individual]:
+                self.interpretation[individual].add(conjunction)
+                changed = True
 
-    for concept in interpretation[individual]:
-        concept_type = concept.getClass().getSimpleName()
+        return changed
 
-        # find conjunctions in individual
-        if concept_type == "ConceptConjunction":
-            for conjunct in concept.getConjuncts():
-                # assign the conjuncts of this conjunction to individual (if not already present)
-                if conjunct not in interpretation[individual]:
-                    interpretation[individual].add(conjunct)
+    def exists_rule_1(self, individual):
+        """
+        # E-rule 1: If d has Er.C assigned, apply E-rules 1.1 and 1.2
+        # E-rule 1.1: If there is an element e with initial concept C assigned, e the r-successor of d.
+        # E-rule 1.2: Otherwise, add a new r-successor to d, and assign to it as initla concept C.
+        """
+        changed = False
+
+        for concept in self.interpretation[individual]:
+            concept_type = concept.getClass().getSimpleName()
+
+            # "If d has Er.C assigned, apply E-rules 1.1 and 1.2"
+            if concept_type == "ExistentialRoleRestriction":
+                role_r = concept.role()  # r of Er.C
+                concept_c = concept.filler()  # C of Er.C
+
+                # E-rule 1.1:
+                # If there is an element e with initial concept C assigned, e is the r-successor of d.
+                if concept_c in self.initial_concepts:
+                    element_e = self.initial_concepts[concept_c]
+                    # TODO: is this check redundant?
+                    if role_r not in self.roles_successors[individual]:
+                        self.roles_successors[individual][role_r] = set()
+                    self.roles_successors[individual][role_r].add(element_e)
                     changed = True
 
-    return changed
+                # E-rule 1.2:
+                # Otherwise, add a new r-successor to d, and assign to it as initial concept C.
+                else:
+                    # TODO: resolve this temp fix for indiviual
+                    new_individual = "d_1"
+                    if role_r not in self.roles_successors[individual]:
+                        self.roles_successors[individual][role_r] = set()
+                    # "add a new r-successor to d"
+                    self.roles_successors[individual][role_r].add(new_individual)
+                    # "and assign to it as initial concept C."
+                    self.initial_concepts[concept_c] = new_individual
+                    changed = True
 
+        return changed
 
-# Intersect rule 2: If d has C intersection D assigned, assign also C intersect D to d
-def intersect_rule_2(individual, interpretation, all_concepts):
-    """
-    For all combinations of concepts in individual, also add the conjunction to the
-    individual. Only do this if the conjunction appears in the Tbox.
-    """
-    changed = False
+    def exists_rule_2(self, individual):
+        # E-rule 2: If d has an r-successor with C assigned, add Er.C to d
+        changed = False
 
-    # get all combinations of 2 for the concepts of this individual
-    individual_concepts = list(interpretation[individual])
-    all_combinations = combinations(individual_concepts, 2)
+        # TODO: I'm not sure if this is correct.
+        for concept in self.interpretation[individual]:
+            for role in self.roles_successors[individual]:
+                if concept in self.roles_successors[individual][role]:
+                    self.interpretation[individual].add(self.elFactory.getExistentialRoleRestriction(role, concept))
+                    changed = True
 
-    # create a conjunction for all combinations
-    for combination in all_combinations:
-        conjunction = elFactory.getConjunction(combination[0], combination[1])
+        return changed
 
-        # assign the conjunction to the individual if it's also in Tbox and not assigned yet
-        if conjunction in all_concepts and conjunction not in interpretation[individual]:
-            interpretation[individual].add(conjunction)
+    def subsumption_rule(self, individual):
+        # Subsumption rule: If d has C assigned and C subsumes D, assign D to d
+        changed = False
+
+        # I tried putting more for-loops in here, i hope this is enough.
+        for concept in self.interpretation[individual]:
+            for subsumed_concept in self.all_concepts:
+                for axiom in self.axioms:
+                    axiom_type = axiom.getClass().getSimpleName()
+                    if (
+                        axiom_type == "GeneralConceptInclusion"
+                        and axiom.lhs() == concept
+                        and axiom.rhs() == subsumed_concept
+                    ):
+                        self.interpretation[individual].add(subsumed_concept)
+                        changed = True
+
+        return changed
+
+    def apply_rules(self, individual):
+        """
+        A function that will apply all rules to individual
+        """
+        pass
+
+    def get_blocked_individuals(self, interpretation):
+        pass
+
+    def run(self):
+        self.subsumers = []
+
+        for concept in self.all_concepts:
+            # reset attributes for this concept
+            subsumer = concept
+            self.first_individual = 1
+            self.last_individual = 1
+            self.initial_concepts = {}
+            self.blocked_individuals = set()
+            self.interpretation = defaultdict(set)
+            self.role_successors = defaultdict(defaultdict(set))
+
+            # 2. set changed == True
             changed = True
+            # 3. while changed == True
+            while changed:
+                # 3.1. set changed == False
+                changed = False
 
-    return changed
+                # 3.2. for every element d in the current interpretation:
+                # 3.2.1. apply all the rules on d in all possible ways,
+                # so that only concepts from the input get assigned
+                for individual in self.interpretation:
+                    if individual in self.get_blocked_individuals(self.interpretation):
+                        # 3.2.2. If a new element was added or a new concept was assigned:
+                        # set changed == True
+                        changed = self.apply_rules(individual)
 
+            # If D_0 was assigned to d_0, return True, else return False
+            if subsumer in self.interpretation[self.first_individual]:
+                self.subsumers.append(subsumer)
 
-def exists_rule_1(individual, interpretation, initial_concepts, roles_successors):
-    """
-    # E-rule 1: If d has Er.C assigned, apply E-rules 1.1 and 1.2
-    # E-rule 1.1: If there is an element e with initial concept C assigned, e the r-successor of d.
-    # E-rule 1.2: Otherwise, add a new r-successor to d, and assign to it as initla concept C.
-    """
-    changed = False
-
-    for concept in interpretation[individual]:
-        concept_type = concept.getClass().getSimpleName()
-
-        # "If d has Er.C assigned, apply E-rules 1.1 and 1.2"
-        if concept_type == "ExistentialRoleRestriction":
-            role_r = concept.role()  # r of Er.C
-            concept_c = concept.filler()  # C of Er.C
-
-            # E-rule 1.1:
-            # If there is an element e with initial concept C assigned, e is the r-successor of d.
-            if concept_c in initial_concepts:
-                element_e = initial_concepts[concept_c]
-                # TODO: is this check redundant?
-                if role_r not in roles_successors[individual]:
-                    roles_successors[individual][role_r] = set()
-                roles_successors[individual][role_r].add(element_e)
-                changed = True
-
-            # E-rule 1.2:
-            # Otherwise, add a new r-successor to d, and assign to it as initial concept C.
-            else:
-                # TODO: resolve this temp fix for indiviual
-                new_individual = "d_1"
-                if role_r not in roles_successors[individual]:
-                    roles_successors[individual][role_r] = set()
-                # "add a new r-successor to d"
-                roles_successors[individual][role_r].add(new_individual)
-                # "and assign to it as initial concept C."
-                initial_concepts[concept_c] = new_individual
-                changed = True
-
-    return changed
-
-
-def exists_rule_2(individual, interpretation, initial_concepts, roles_successors):
-    # E-rule 2: If d has an r-successor with C assigned, add Er.C to d
-    changed = False
-
-    # TODO: I'm not sure if this is correct.
-    for concept in interpretation[individual]:
-        for role in roles_successors[individual]:
-            if concept in roles_successors[individual][role]:
-                interpretation[individual].add(elFactory.getExistentialRoleRestriction(role, concept))
-                changed = True
-
-    return changed
-
-
-def subsumption_rule(individual, interpretation, allConcepts, axioms):
-    # Subsumption rule: If d has C assigned and C subsumes D, assign D to d
-    changed = False
-
-    # I tried putting more for-loops in here, i hope this is enough.
-    for concept in interpretation[individual]:
-        for subsumed_concept in allConcepts:
-            for axiom in axioms:
-                axiom_type = axiom.getClass().getSimpleName()
-                if (
-                    axiom_type == "GeneralConceptInclusion"
-                    and axiom.lhs() == concept
-                    and axiom.rhs() == subsumed_concept
-                ):
-                    interpretation[individual].add(subsumed_concept)
-                    changed = True
-
-    return changed
-
-
-## --- // -----------------------------------------------------------------------------
-## EL Completion algorithm to decide whether O entails C subsumes D
-
-elFactory = gateway.getELFactory()
-
-# 1. start with initial element d_0, assign C_0 to it as initial concept
-d_0 = "d_0"
-CLASS_NAME = "C"
-
-# Initial concepts: Key = initial concept, Value = individual
-initial_concepts = defaultdict(str)
-
-# interpretation: Key = individual, Value = set of Concepts
-interpretation = defaultdict(set)
-
-# roles_successors: {individual : {role : {succesors}}}
-roles_successors = defaultdict(defaultdict(set()))
-
-# Assign C to d_0, where C is the CLASS_NAME given at the commandline
-initial_concepts[CLASS_NAME] = d_0
-interpretation[d_0].add(CLASS_NAME)
-
-# 2. set changed == True
-changed = True
-# 3. while changed == True
-while changed:
-    # 3.1. set changed == False
-    changed = False
-    # 3.2. for every element d in the current interpretation:
-    for d in interpretation:
-        print(d)
-        # 3.2.1. apply all the rules on d in all possible ways,
-        # so that only concepts from the input get assigned
-
-        # 3.2.2. If a new element was added or a new concept was assigned:
-        # set changed == True
-
-# If D_0 was assigned to d_0, return True, else return False
+        return self.subsumers
